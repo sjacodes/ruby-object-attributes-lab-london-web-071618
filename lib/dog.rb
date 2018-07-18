f86b64d6677806717c18dc6a23f2117b5a1cdc2f
Sarah Jacob <smjacob91@gmail.com>
2018-07-18 05:57:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,5 @@
 class Dog
+  
   def initialize
     @name = name
     @breed = breed
@@ -8,64 +9,18 @@ class Dog
     @name
   end
   
-  def name=(new_name)
-    @name=new_name
+  def name = (new_name)
+    @name = new_name
   end
   
   def breed
     @breed
   end
   
-  def breed=(new_breed)
-    @breed=
-    
-
-  
-  
-  
-  
+  def breed = (new_breed)
+    @breed = new_breed
+  end
   
 end
   
   
-  
-  context 'with names' do
-    describe '#name=' do
-      it 'writes the name of the dog to an instance variable @name' do
-        fido = Dog.new
-        fido.name = "Fido"
-
-        expect(fido.instance_variable_get(:@name)).to eq("Fido")
-      end
-    end
-
-    describe '#name' do
-      it 'reads the name of the dog from an instance variable @name' do
-        fido = Dog.new
-        fido.instance_variable_set(:@name, "Fido")
-
-        expect(fido.name).to eq("Fido")
-      end
-    end
-  end
-
-  context 'with breeds' do
-    describe '#breed=' do
-      it 'writes the breed of the dog to an instance variable @breed' do
-        snoopy = Dog.new
-        snoopy.breed = "Beagle"
-
-        expect(snoopy.instance_variable_get(:@breed)).to eq("Beagle")
-      end
-    end
-
-    describe '#breed' do
-      it 'reads the breed of the dog from an instance variable @breed' do
-        snoopy = Dog.new
-        snoopy.instance_variable_set(:@breed, "Beagle")
-
-        expect(snoopy.breed).to eq("Beagle")
-      end
-    end
-  end
-end
