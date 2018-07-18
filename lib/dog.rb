17277fe769225d088bdeeb6852d8529f601ee616
Sarah Jacob <smjacob91@gmail.com>
2018-07-18 05:51:18 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,3 +6,49 @@ class Dog
   end
 end
 
+describe 'Dog' do
+  it 'is defined within lib/dog.rb' do
+    expect(defined?(Dog)).to be_truthy
+    expect(Dog).to be_a(Class)
+  end
+
+  context 'with names' do
+    describe '#name=' do
+      it 'writes the name of the dog to an instance variable @name' do
+        fido = Dog.new
+        fido.name = "Fido"
+
+        expect(fido.instance_variable_get(:@name)).to eq("Fido")
+      end
+    end
+
+    describe '#name' do
+      it 'reads the name of the dog from an instance variable @name' do
+        fido = Dog.new
+        fido.instance_variable_set(:@name, "Fido")
+
+        expect(fido.name).to eq("Fido")
+      end
+    end
+  end
+
+  context 'with breeds' do
+    describe '#breed=' do
+      it 'writes the breed of the dog to an instance variable @breed' do
+        snoopy = Dog.new
+        snoopy.breed = "Beagle"
+
+        expect(snoopy.instance_variable_get(:@breed)).to eq("Beagle")
+      end
+    end
+
+    describe '#breed' do
+      it 'reads the breed of the dog from an instance variable @breed' do
+        snoopy = Dog.new
+        snoopy.instance_variable_set(:@breed, "Beagle")
+
+        expect(snoopy.breed).to eq("Beagle")
+      end
+    end
+  end
+end
